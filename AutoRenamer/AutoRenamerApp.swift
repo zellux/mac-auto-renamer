45cc14fce49d5b879ca3bc64770282be1fb83b3a
Zellux Wang <zellux@gmail.com>
2026-02-10 10:38:52 -0800

    Initial Commit

---
+++ AutoRenamer/AutoRenamerApp.swift
@@ -0,0 +1,17 @@
+//
+//  AutoRenamerApp.swift
+//  AutoRenamer
+//
+//  Created by Zellux Wang on 2/10/26.
+//
+
+import SwiftUI
+
+@main
+struct AutoRenamerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AutoRenamer/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AutoRenamer
+//
+//  Created by Zellux Wang on 2/10/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
